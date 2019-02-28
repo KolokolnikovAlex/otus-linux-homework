3165b6a1e79e69c57e5e0c78113e970d5bbfec1a
Alexey Kolokolnikov <alexey.kolokolnikov@datanerds.ai>
2019-02-28 23:57:11 +0300

    homework9 fix stderror

--- hmwork09/ps_shell.sh
+++ hmwork09/ps_shell.sh
@@ -3,7 +3,10 @@ do
 state=$(grep -E -h -s -i "state:\s(.+)" "/proc/"$pid"/status" | sed 's|State:||')
 ppid=$(grep -E -h -s -i "ppid:\s(.+)" "/proc/"$pid"/status" | grep -o "[0-9]\+")
 sleepavg=$(grep -E -h -s -i "avg_atom(.+)" "/proc/"$pid"/sched" | grep -o "[0-9.]\+")
-execcmd=$(tr '\0' ' ' < "/proc/"$pid"/cmdline")
+execcmd=""
+if [[ -f "/proc/"$pid"/cmdline" ]]
+then execcmd=$(tr '\0' ' ' < "/proc/"$pid"/cmdline")
+fi
 if [[ -z $sleepavg ]]
 then sleepavg=0
 fi
